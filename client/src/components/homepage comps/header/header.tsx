35b563ee0f99804533af9300712bfc33a852bd8a
[200~omarelsabagh <omarhamid4work@yahoo.com>
2023-02-18 10:02:53 +0200

    finish nav2

--- client/src/components/homepage comps/header/header.tsx
+++ client/src/components/homepage comps/header/header.tsx
@@ -1,10 +1,13 @@
 
 import Navbar from "./navbar/navbar"
+import Navbar2 from "./navbar2/navbar2"
 function Header(){
 return (
     <>
     <header>
     <Navbar/>
+    <Navbar2/>
+
     </header>
 
     </>
---
+++ client/src/components/homepage comps/header/navbar2/navbar2.tsx
@@ -0,0 +1,36 @@
+function Navbar2(){
+
+    return <>
+    <nav>
+        <div className="bg-black p-3 ">
+        <ul className="flex items-center text-white gap-x-12 mx-6  cursor-pointer font-semibold">
+            <li className="border-b-2 border-transparent hover:border-white">
+                <a href="">SHOP</a>
+            </li>
+            <li className="border-b-2 border-transparent hover:border-white">
+                <a href="">PRODUCT</a>
+            </li>
+            <li className="border-b-2 border-transparent hover:border-white">
+                <a href="">BLOG</a>
+            </li>
+            <li className="border-b-2 border-transparent hover:border-white">
+                <a href="">PAGES</a>
+            </li>
+            <li className="border-b-2 border-transparent hover:border-white">
+                <a href="">NEW IN</a>
+            </li>
+            <li className="border-b-2 border-transparent hover:border-white">
+                <a href="">TREND</a>
+            </li>
+            <li className="border-b-2 border-transparent hover:border-white">
+                <a href="">COLLECTIONS</a>
+            </li>
+        </ul>
+        </div>
+
+    </nav>
+    </>
+}
+
+
+export default Navbar2
