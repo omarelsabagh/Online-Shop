775dab41c7ef860b7f1d7b61a8516598730dc94e
[200~omarelsabagh <omarhamid4work@yahoo.com>
2023-02-19 11:51:31 +0200

    finish transition

--- client/src/components/homepage comps/homepage.tsx
+++ client/src/components/homepage comps/homepage.tsx
@@ -1,10 +1,14 @@
 import Header from "./header/header"
 import Section2 from "./section2/section2"
+import Transition from "./transition/transitions"
+
  function Homepage() {
+  const transitions  = {newArrivals:"NEW ARRIAVALS"}
   return (
     <>
    <Header/>
    <Section2/>
+   <Transition transName={transitions.newArrivals}/>
     </>
   )
 }
