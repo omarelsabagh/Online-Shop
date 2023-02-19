699b662617769a60d7eed8056d348b5f4d9c81d5
[200~omarelsabagh <omarhamid4work@yahoo.com>
2023-02-19 12:28:06 +0200

    remove console2

--- client/src/components/homepage comps/homepage.tsx
+++ client/src/components/homepage comps/homepage.tsx
@@ -1,17 +1,17 @@
-import Header from "./header/header"
-import Section2 from "./section2/section2"
-import Transition from "./transition/transitions"
+import Header from "./header/header";
+import Section2 from "./section2/section2";
+import Transition from "./transition/transitions";
 
- function Homepage() {
-  const transitions   = {newArrivals:"NEW ARRIAVALS"}
+function Homepage() {
+  const transitions = { newArrivals: "NEW ARRIAVALS" };
 
   return (
     <>
-   <Header/>
-   <Section2/>
-   <Transition transName={transitions.newArrivals}/>
+      <Header />
+      <Section2 />
+      <Transition transName={transitions.newArrivals} />
     </>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
